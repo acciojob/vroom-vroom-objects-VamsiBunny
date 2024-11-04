f6f373bf2e4c8315765e5ccc577c4911d4557264
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2024-11-04 16:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// Complete the js code
 function Car(make, model) {
 	this.make=make;
 	this.model=model;
 }
-Car.prototype.getMakeModel => (){
+Car.prototype.getMakeModel =function() {
 	return `${this.make}, ${this.model}`;
 	
 }
@@ -15,10 +14,10 @@ function SportsCar(make, model, topSpeed) {
 SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 
-SportsCar.prototype.getTopSpeed=> (){
+SportsCar.prototype.getTopSpeed=function(){
 	return this.topSpeed;
 }
 
-// Do not change the code below
+
 window.Car = Car;
 window.SportsCar = SportsCar;
